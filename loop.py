e48e2a0a33cea8851390a0c7bf65d6b6e6a21085
Md Golam Maula <maula03@gmail.com>
2025-11-16 12:01:52 +0600

    added class-21

---
+++ loop.py
@@ -0,0 +1,39 @@
+# new = [ 1, 2, 4, 6, 7]
+# newEven = []
+
+# for index in range(len(new)):
+#     if new[index] % 2 == 0:
+#         newEven.append(new[index] ** 2)
+
+# print(newEven) # 4, 16, 36
+
+
+# fruits = [ "apple", "banana", "cherry"]
+
+# # for i in range(len(fruits)):
+# #     print(fruits[i])
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+# numList = [1, 5, 67, 78, 84]
+# total = 0
+# print(numList)
+# for num in numList:
+#     # print(f" Total before addition: {total} ")
+#     # print(f"{total} + {num} = {total + num}")
+#     total += num
+    
+# print(total)
+
+
+# nameList = ["shuvo hossen", "sultana islam isha", "samad islam"]
+
+# #Shuvo Hossen
+
+# for name in nameList:
+#     print(name.title())
+    
+
+newList = 
